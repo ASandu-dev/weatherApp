41770eec30c284ddf623001c3bfca09bd5595fc4
sandu <sandu.andrei86@gmail.com>
2022-03-02 10:43:12 +0000

    functional app stage

--- script.js
+++ script.js
@@ -11,4 +11,44 @@ const url = (city) =>
 async function getWeatherByLocation(city){
     const response = await fetch(url(city), {origin: "cors"});
     const responseData = await response.json();
+
+    console.log(responseData);
+
+    addWeatherToPage(responseData);
 }    
+
+
+
+function addWeatherToPage(data){
+    const temp = KtoC(data.main.temp);
+
+    const weather = document.createElement("div");
+    weather.classList.add("weather");
+
+    weather.innerHTML = 
+    `
+    <h2><img src="https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png"/> ${temp}°C <img src="https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png"/> </h2>
+    <small>${data.weather[0].main}</small>
+    `;
+
+    //cleanup
+    main.innerHTML= "";
+    main.appendChild(weather);
+
+    
+}
+
+function KtoC(K){
+    return Math.floor(K - 273.15);
+}
+
+form.addEventListener("submit", (e) =>{
+    e.preventDefault();
+
+    const city = search.value;
+
+    if(city){
+        getWeatherByLocation(city);
+    }
+});
+
